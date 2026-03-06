0fa9b6a5d6e2113238e160a7eccf10dc917e3867
ez-sourav <souravb2003@gmail.com>
2026-03-06 19:27:57 +0530

    modify login page

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import '../auth.form.scss'
 export const Login = () => {
   return (
-    <div>login</div>
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form>
+    <div className="input-group">
+      <label htmlFor="email">Email</label>
+      <input type="email" name="email" id="email" placeholder='Enter email address' />
+    </div>
+    <div className="input-group">
+      <label htmlFor="password">Password</label>
+      <input type="password" name="password" id="password" placeholder='Enter password' />
+    </div>
+    <button className='button primary-button'>Login</button>
+        </form>
+      </div>
+    </main>
   )
 }
