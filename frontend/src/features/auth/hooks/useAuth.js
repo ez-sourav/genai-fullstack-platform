9ede2a5335a4bfd62065e3744ec6b90e1050c8af
ez-sourav <souravb2003@gmail.com>
2026-03-08 14:56:54 +0530

    improve error handling using try catch block

--- frontend/src/features/auth/hooks/useAuth.js
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -8,23 +8,44 @@ export const useAuth = ()=>{
 
     const handleLogin = async ({email,password}) =>{
         setLoading(true)
-        const data = await login({email,password})
-        setUser(data.user)
-        setLoading(false)
+        try{
+
+            const data = await login({email,password})
+            setUser(data.user)
+        }catch(error){
+
+        }finally{
+            
+            setLoading(false)
+        }
     }
 
     const handleRegister = async ({username,email,password})=>{
         setLoading(true);
-        const data =await register({username,email,password})
-        setUser(data.user)
-        setLoading(false)
+        try {
+            
+            const data =await register({username,email,password})
+            setUser(data.user)
+        } catch (error) {
+            
+        }finally{
+
+            setLoading(false)
+        }
     }
 
     const handleLogout = async ()=>{
         setLoading(true);
-        const data = await logout();
-        setUser(null)
-        setLoading(false)
+        try{
+
+            const data = await logout();
+            setUser(null)
+        }catch(error){
+
+        }finally{
+
+            setLoading(false)
+        }
     }
     
     return {user,loading,handleRegister,handleLogin,handleLogout}
