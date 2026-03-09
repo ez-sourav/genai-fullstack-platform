17cdd974956c5c6e7f31a0703a916828ceaf07ca
ez-sourav <souravb2003@gmail.com>
2026-03-09 20:00:12 +0530

    add login page

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import {useState} from 'react'
 import '../auth.form.scss'
 import { Link, useNavigate } from 'react-router'
+import {useAuth} from '../hooks/useAuth'
+
 export const Login = () => {
-  const navigate = useNavigate();
-   const handleSubmit = (e)=>{
+  const {loading,handleLogin} = useAuth()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+  const handleSubmit =async (e) => {
     e.preventDefault()
-   }
+    await handleLogin({email,password})
+    navigate('/')
+  }
+
+  if(loading){
+    return(
+      <main>
+        <h1>Loading...</h1>
+      </main>
+    )
+  }
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
         <form onSubmit={handleSubmit}>
-    <div className="input-group">
-      <label htmlFor="email">Email</label>
-      <input type="email" name="email" id="email" placeholder='Enter email address' />
-    </div>
-    <div className="input-group">
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" placeholder='Enter password' />
-    </div>
-    <button className='button primary-button'>Login</button>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input value={email} onChange={(e)=>{setEmail(e.target.value)}}  type="email" name="email" id="email" placeholder='Enter email address' />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input value={password} onChange={(e)=>{setPassword(e.target.value)}}  type="password" name="password" id="password" placeholder='Enter password' />
+          </div>
+          <button className='button primary-button'>Login</button>
         </form>
         <p>Don't have an account? <Link to='/register'>Register</Link></p>
       </div>
