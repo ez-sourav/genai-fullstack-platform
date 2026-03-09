d39acbc37c694ba0e1d0c5e422c6860864ef6b15
ez-sourav <souravb2003@gmail.com>
2026-03-09 19:58:11 +0530

    add protected component

---
+++ frontend/src/features/auth/components/Protected.jsx
@@ -0,0 +1,16 @@
+import {  Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+export const Protected = ({children}) => {
+    const {loading,user} = useAuth();
+    if(loading){
+        return(
+            <main><h1>loading...</h1></main>
+        )
+    }
+
+    if(!user){
+        return <Navigate to={'/login'}/>
+    }
+  return children
+}
