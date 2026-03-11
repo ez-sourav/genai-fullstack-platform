90c7a0a89c60bdc3360d4c80ad0a81f381c11b0a
ez-sourav <souravb2003@gmail.com>
2026-03-11 21:28:28 +0530

    add api

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 const app = require('./src/app')
 const { connectToDB } = require('./src/config/database')
+const { resume, selfDescription, jobDescription } = require('./src/services/temp')
+const {generateInterviewReport} = require('./src/services/ai.service')
 
 connectToDB()
+// invokeGeminiAi()
+generateInterviewReport({ resume, selfDescription, jobDescription })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
