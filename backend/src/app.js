4bd4c1e77bdf1a5742ef80bc7d9aaf8d7e246abf
ez-sourav <souravb2003@gmail.com>
2026-03-13 10:27:22 +0530

    add route interview

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express(); 
 const authRouter = require('./routes/auth.routes')
 const cookieParser = require('cookie-parser');
-const cors = require('cors')
+const cors = require('cors');
+const interviewRouter = require('./routes/interview.routes');
 
 app.use(cookieParser())
 
@@ -14,5 +15,6 @@ app.use(cors({
 }))
 
 app.use('/api/auth',authRouter)
+app.use('/api/interview',interviewRouter)
 
 module.exports = app
