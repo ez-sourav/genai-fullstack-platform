e2d8d8177b29bf0ed49feac50b6eccad6a97853f
ez-sourav <souravb2003@gmail.com>
2026-03-25 12:22:43 +0530

    change loading state

--- frontend/src/features/interview/interview.context.jsx
+++ frontend/src/features/interview/interview.context.jsx
@@ -7,7 +7,7 @@ export const InterviewProvider = ({ children }) => {
     // const [loading, setLoading] = useState(false);
     const [isGenerating, setIsGenerating] = useState(false);
     const [isFetchingReports, setIsFetchingReports] = useState(false);
-    const [isFetchingSingle, setIsFetchingSingle] = useState(false);
+    const [isFetchingSingle, setIsFetchingSingle] = useState(true);
     const [report, setReport] = useState(null);
     const [reports, setReports] = useState([]);
     const [error, setError] = useState(null);
